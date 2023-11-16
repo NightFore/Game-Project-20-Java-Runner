5c419eb649ec2bd3938afb8eb4fd7be75a5b6a14
NightFore <Et3rnNighTaLL@Hotmail.com>
2023-11-16 17:26:00 +0100

    Version 0.0.1f: Heart, numberOfLives

--- src/main/java/com/example/gameproject20javarunner/GameScene.java
+++ src/main/java/com/example/gameproject20javarunner/GameScene.java
@@ -1,5 +1,3 @@
-// GameScene.java
-
 package com.example.gameproject20javarunner;
 
 import javafx.scene.Scene;
@@ -9,18 +7,34 @@ public class GameScene extends Scene {
     private Camera camera;
     private StaticThing backgroundLeft;
     private StaticThing backgroundRight;
+    private Heart[] hearts;
 
     // Constructor taking the camera, the main container, and the dimensions of the scene
     public GameScene(Camera camera, Pane root, double width, double height) {
         super(root, width, height);
         this.camera = camera;
 
-        // Instantiate StaticThings to represent the background (one on the left and one on the right)
+        // Instantiate static elements to represent the background (one on the left and one on the right)
         backgroundLeft = new StaticThing(800, 600, "/img/desert.png");
         backgroundRight = new StaticThing(800, 600, "/img/desert.png");
 
-        // Add the ImageViews of StaticThings to the main container
+        // Add the ImageViews of static elements to the main container
         root.getChildren().addAll(backgroundLeft.getImageView(), backgroundRight.getImageView());
+
+        // Initial number of lives
+        int numberOfLives = 3;
+
+        // Instantiate hearts to indicate the initial number of lives
+        hearts = new Heart[numberOfLives];
+        for (int i = 0; i < numberOfLives; i++) {
+            // Position the hearts at a certain distance from each other
+            double heartX = i * Heart.getSize();
+            double heartY = 20.0;
+
+            // Instantiate a heart and add it to the main container
+            hearts[i] = new Heart(heartX, heartY);
+            root.getChildren().add(hearts[i].getImageView());
+        }
     }
 
     // Getter for the camera
@@ -34,11 +48,11 @@ public class GameScene extends Scene {
         double cameraX = camera.getX();
         double cameraY = camera.getY();
 
-        // Adjust the position of the ImageViews of StaticThings based on the camera
-        backgroundLeft.getImageView().setX(cameraX - getWidth() / 2);
-        backgroundLeft.getImageView().setY(cameraY);
+        // Adjust the position of the ImageViews of static elements based on the camera
+        backgroundLeft.getImageView().setTranslateX(cameraX - getWidth() / 2);
+        backgroundLeft.getImageView().setTranslateY(cameraY);
 
-        backgroundRight.getImageView().setX(cameraX + getWidth() / 2);
-        backgroundRight.getImageView().setY(cameraY);
+        backgroundRight.getImageView().setTranslateX(cameraX + getWidth() / 2);
+        backgroundRight.getImageView().setTranslateY(cameraY);
     }
 }
---
+++ src/main/java/com/example/gameproject20javarunner/Heart.java
@@ -0,0 +1,44 @@
+package com.example.gameproject20javarunner;
+
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Objects;
+
+public class Heart {
+    private static final double SIZE = 32.0;
+    private final ImageView imageView;
+
+    public Heart(double x, double y) {
+        // Load heart sprite sheet image from resources
+        Image heartSpriteSheet = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/img/NicoleMarieT_Heart_Sprite_Sheet_32x32.png")));
+
+        // Create an ImageView with the heart sprite sheet
+        imageView = new ImageView(heartSpriteSheet);
+
+        // Set the size of the ImageView
+        imageView.setFitWidth(SIZE);
+        imageView.setFitHeight(SIZE);
+
+        // Define the rectangle for the full heart frame on the sprite sheet
+        Rectangle2D fullHeartFrame = new Rectangle2D(0, 0, SIZE, SIZE);
+
+        // Set the viewport of the ImageView to display the full heart frame
+        imageView.setViewport(fullHeartFrame);
+
+        // Position the heart
+        imageView.setTranslateX(x);
+        imageView.setTranslateY(y);
+    }
+
+    // Getter for the heart's ImageView
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    // Getter for the SIZE constant
+    public static double getSize() {
+        return SIZE;
+    }
+}
